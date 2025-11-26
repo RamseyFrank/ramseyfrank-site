0a43bc4499a0f59468caa602b3a091bd61c83b10
RamseyFrank <ramseycfrank@gmail.com>
2025-11-26 14:55:04 -0600

    messing with the waves

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,21 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div>
-      <h1>Hello, world!</h1>
+    <div className="App">
+      <h1>Hello, you!</h1>
       <button onClick={() => setCount(count + 1)}>
         Count is {count}
       </button>
+
+      {/* Wave bars - outside of button */}
+      <div className="wave">
+        {Array.from({ length: 10 }).map((_, i) => (
+          <span key={i}></span>
+        ))}
+      </div>
     </div>
   );
 }
 
 export default App;
+
